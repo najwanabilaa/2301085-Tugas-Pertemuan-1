68b2fc9d2b3e8674004f81d7b3d0f9ab59174485
Najwa Nabila H <162705772+najwanabilaa@users.noreply.github.com>
2025-02-09 01:32:11 +0700

    Create HeloWorld.java

---
+++ HeloWorld.java
@@ -0,0 +1,6 @@
+public class HeloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hallo Word Najwa Nabila dengan NIM 2301085 Saya Senang Belajar Java");
+    }
+}
+
